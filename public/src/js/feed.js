d4e9c87a06701f7a2fd36e24c823edaf3fe05804
iamsss <saurav9760@gmail.com>
2018-07-02 13:06:47 +0530

    Displaying prompt as deferred

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,20 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  
+  if(deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if(choiceResult.outcome === 'dismissed') {
+        console.log('User Cancelled the installation');
+      } else {
+        console.log('User Install the prompt');
+      }
+    });
+  }
 }
 
 function closeCreatePostModal() {
