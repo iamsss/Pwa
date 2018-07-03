b53d9f33ab14f5a03aa67ac16e728d66908dc12b
iamsss <saurav9760@gmail.com>
2018-07-03 09:18:45 +0530

    Caching Multiple Static File

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,24 @@ self.addEventListener('install', function(event){
         caches.open('static')
             .then(function(cache){
                 console.log(' Precaching App Shell');
-                cache.add('/src/js/app.js');
+                // cache.add('/src/js/app.js');
+                // cache.add('/index.html');
+                
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ])
             })
     )
 });
@@ -14,6 +31,14 @@ self.addEventListener('activate', function(event){
 });
 
 self.addEventListener('fetch', function(event){
-
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request) //To match current request with cached request it
+		.then(function(response) {
+			//If response found return it, else fetch again.
+			return response || fetch(event.request);
+		})
+		.catch(function(error) {
+			console.log("Error: ", error);
+		})
+    )
 });
